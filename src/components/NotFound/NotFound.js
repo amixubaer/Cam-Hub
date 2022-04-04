50818684e19f9efc5fe3f87ae45c0497ddafcedf
Abu Jubaer <abujubaer90@gmail.com>
2022-04-04 23:49:13 +0600

    rating style added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404</h1>
-      <h1>Page Not Found.</h1>
+    <div className="text-center">
+      <h1 className="text-5xl font-bold my-2">404</h1>
+      <h1 className="text-5xl font-bold my-2">Page Not Found.</h1>
     </div>
   );
 };
--- src/components/Reviews/Review.js
+++ src/components/Reviews/Review.js
@@ -1,13 +1,22 @@
 import React from 'react';
-
+import Rating from "react-rating";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Review = (props) => {
     const {name, comment, rating } = props.review;
     return (
         <div className='block p-6 m-10 bg-white rounded-lg border border-gray-200 shadow-md hover:bg-gray-100 dark:bg-gray-200 dark:border-gray-700 dark:hover:bg-gray-700 text-center'>
-            <h1 className='font-bold'>Name: {name}</h1>
-            <h1>Comment: {comment}</h1>
-            <h1>Rating: {rating}</h1>
+            <h1 className='font-bold'> {name}</h1>
+            <h1> {comment}</h1>
+            <Rating
+          initialRating={rating}
+          emptySymbol={<FontAwesomeIcon icon={faStar} />}
+          fullSymbol={
+            <FontAwesomeIcon style={{ color: "goldenrod" }} icon={faStar} />
+          }
+          readonly
+        ></Rating>
 
         </div>
     );
